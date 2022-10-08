ef7b1249cc8694de45905a28e6d2ded95f0a640d
ElisabeteXavier <elisabetebx@gmail.com>
2022-10-08 18:16:46 -0300

    adiciona endpoint de teste

--- src/main/java/com/netflix/api/controller/HelloWorldController.java
+++ src/main/java/com/netflix/api/controller/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("say-hello")
-    public ResponseEntity<?> helloWorld(){
+    public ResponseEntity<String> helloWorld(){
         return ResponseEntity.ok("Hello World");
     }
 
