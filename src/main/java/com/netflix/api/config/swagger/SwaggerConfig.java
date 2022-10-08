4ae59a1c09e1496a065b68e04da944a34b7df2ae
ElisabeteXavier <elisabetebx@gmail.com>
2022-10-08 18:14:42 -0300

    adiciona swagger

---
+++ src/main/java/com/netflix/api/config/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.netflix.api.config.swagger;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Netflix API")
+                        .description("")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("")
+                        .url(""));
+    }
+}
--- src/main/java/com/netflix/api/controller/AuthController.java
+++ src/main/java/com/netflix/api/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.netflix.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
 public class AuthController {
-
-
 
 }
